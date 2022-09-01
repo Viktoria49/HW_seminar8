a5ccc12201b39074e30daa4203c04f413dd8de5b
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-09-01 23:38:43 +0700

    .\Task_60\

---
+++ Task_60/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] Create3DArrayRndInt(int z, int x, int y, int min, int max)
+{
+    int[,,] array = new int[z, x, y];
+    Random rnd = new Random((int)DateTime.Now.Millisecond);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i,j,k] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+
+    return array;
+}
+
+void Print3DArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] array3d = Create3DArrayRndInt(2, 2, 2, 10, 99);
+Print3DArray(array3d);
+Console.WriteLine();
